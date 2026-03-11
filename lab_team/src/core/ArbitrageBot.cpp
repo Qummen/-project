34511d4ebbbeef22ee1421b68671744cde637c50
Qummen <spirin.artin@gmail.com>
2026-03-11 10:33:16 +0300

    Fix exchange removal logic in findArbitrage
    Fixed duplicate removal of buyExchange and added removal of sellExchange.

--- lab_team/src/core/ArbitrageBot.cpp
+++ lab_team/src/core/ArbitrageBot.cpp
@@ -44,15 +44,25 @@ std::vector <std::pair<Order, Order>>  ArbitrageBot::findArbitrage() {
                 exchanges_.end(),
                 [buyExchange](const std::unique_ptr<Exchange>& ex) {
                     return ex.get() == buyExchange;
-        }
-    ),
-    exchanges_.end()
-);
+            } 
+            ), exchanges_.end() 
+            );
+        
+        exchanges_.erase(
+            std::remove_if(
+                exchanges_.begin(),
+                exchanges_.end(),
+                [buyExchange](const std::unique_ptr<Exchange>& ex) {
+                    return ex.get() == sellExchange;
+            } 
+            ), exchanges_.end() 
+            );
     }
 
     return goodPairs;
 
 }
+
 
 
 
