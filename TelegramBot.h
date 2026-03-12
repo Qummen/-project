f67218c8f57924cd4e7e5fcda86cb74f5f640cfd
PiskunovHleb <118543397+PiskunovHleb@users.noreply.github.com>
2026-03-12 09:32:55 +0300

    add telegram bot interface
    Removed httpPost method declaration from TelegramBot class.

--- TelegramBot.h
+++ TelegramBot.h
@@ -10,7 +10,6 @@ namespace telegram {
     class TelegramBot {
     public:
         explicit TelegramBot(std::string token);
-
         void run();
 
     private:
@@ -22,7 +21,6 @@ namespace telegram {
         static std::size_t writeCallback(void* contents, std::size_t size, std::size_t nmemb, void* userp);
 
         std::string httpGet(const std::string& url) const;
-        std::string httpPost(const std::string& url, const std::string& jsonBody) const;
 
         void pollUpdates();
         void sendMessage(std::int64_t chatId, const std::string& text) const;
