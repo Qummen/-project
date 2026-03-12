8c31dbd9c1696ecc87261a64dfbbe7c374da9f5d
PiskunovHleb <118543397+PiskunovHleb@users.noreply.github.com>
2026-03-12 13:28:50 +0300

    add callback handler support

--- CommandHandler.h
+++ CommandHandler.h
@@ -7,6 +7,7 @@ namespace telegram {
     class CommandHandler {
     public:
         [[nodiscard]] std::string handleMessage(const std::string& text) const;
+        [[nodiscard]] std::string handleCallback(const std::string& data) const;
     };
 
 }
