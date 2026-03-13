902c21ffab5f4768c1be38a8fa6e895b3850e35d
MalyuginMatvey <malyuginmatvey1@gmail.com>
2026-03-13 14:09:51 +0300

    bench_arbitrage.cpp

--- Final_project/benchmarks/bench_arbitrage.cpp
+++ Final_project/benchmarks/bench_arbitrage.cpp
@@ -1,19 +1,16 @@
 #include <benchmark/benchmark.h>
-
 #include "core/ArbitrageBot.h"
 #include "exchanges/Bybit.h"
 #include "exchanges/OKX.h"
 
-static void BM_FindArbitrage(benchmark::State& state){
-    for (auto _ : state){
+static void BM_FindArbitrage(benchmark::State& state) {
+    for (auto _ : state) {
         ArbitrageBot bot;
 
-        bot.addExchange(Bybit::create("BTCUSDT", 100.0, 101.0));
-        bot.addExchange(OKX::create("BTCUSDT", 100.5, 100.9));
+        bot.addExchange(Bybit::create("BTCUSDT", 80.0, 100.0)); 
+        bot.addExchange(OKX::create("BTCUSDT", 120.0, 140.0));
 
         benchmark::DoNotOptimize(bot.findArbitrage());
     }
 }
 BENCHMARK(BM_FindArbitrage);
-
-BENCHMARK_MAIN();
